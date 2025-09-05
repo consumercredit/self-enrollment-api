23dcf94f22e8dc0895a7f2ee9080585a56d78be0
Alex Westcott <51130014+adwestcott@users.noreply.github.com>
2025-09-05 12:17:45 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -56,7 +56,7 @@ app.use(cors({
 
 app.get('/refHowDidYouHearAboutUs', async (req, res) => {
   try {
-    const result = await db.raw('EXEC get_refHowDidYouHearAboutUs');
+    //const result = await db.raw('EXEC get_refHowDidYouHearAboutUs');
     res.json("chubby dubby doo!");
   } catch (err: any) {
     res.status(500).json({ error: 'Database error', details: err.message });
